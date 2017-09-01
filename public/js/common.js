304a513ceb12fd502e6876ca6fc30c30c52cdfd3
foreezh <522292344@qq.com>
2017-09-01 09:50:28 +0800

    模版引擎重构登录功能

--- public/js/common.js
+++ public/js/common.js
@@ -1,6 +1,5 @@
-define(['jquery','cookie'],function($){
+define(['jquery','template','cookie'],function($,template){
 	// NProgress.start();
-
 	// NProgress.done();
 	// 控制左侧菜单的折叠和展开
 	$('.navs ul').prev('a').on('click', function () {
@@ -33,8 +32,13 @@ define(['jquery','cookie'],function($){
 	var loginInfo = $.cookie('loginInfo');
 	//console.log(loginInfo);
 	var info = JSON.parse(loginInfo);
-	$('.profile img').attr('src',info.tc_avatar);
-	$('.profile h4').html(info.tc_name);
+
+  	var tpl = '<div class="avatar img-circle"><img src="{{tc_avatar}}"></div><h4>{{tc_name}}</h4>';
+
+  	var html = template.render(tpl,info);
+  	$('.aside .profile').html(html);
+	// $('.profile img').attr('src',info.tc_avatar);
+	// $('.profile h4').html(info.tc_name);
 });
 
 
